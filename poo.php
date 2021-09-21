5b2664ffcea99b7757176a29710ccf151d1f3885
Caio Couto <coutocaio3@gmail.com>
2021-09-20 21:45:28 -0300

    Adicionando o método Destrutor na classe Pessoa

--- poo.php
+++ poo.php
@@ -21,10 +21,20 @@ class Pessoa {
 
     public function __construct($n, $c) {
         // É chamado toda vez que a classe é instanciada.
+        // chamado implicitamente.
         $this->nome = $n;
         $this->cpf = $c;
         echo 'Um objeto da classe ' . __CLASS__ . ' foi criado!' . PHP_EOL;
         echo 'Olá! Eu sou um objeto, e meu nome é: '. $this->nome . PHP_EOL;
+    }
+
+    public function __destruct() {
+        /**
+         * Executa em duas ocasiões
+         * 1 - O script acaba.
+         * 2 - O objeto é destruído.
+         */
+        echo "Destrutor da classe " . __CLASS__ . " foi executado" . PHP_EOL;
     }
 
     // Métodos
@@ -46,9 +56,13 @@ class Estudante extends Pessoa {
     }
 }
 
-// $obj = new Pessoa('Caio', '11111111111'); // instanciando a classe Pessoa (criando um novo objeto do tipo Pessoa)
+$obj = new Pessoa(
+    c:'11111111111',
+    n:'Caio'
+); // instanciando a classe Pessoa (criando um novo objeto do tipo Pessoa)
 // $obj2 = new Pessoa('Marcos', '22222222222'); // instanciando a classe Pessoa (criando um novo objeto do tipo Pessoa)
-$objEstudante = new Estudante('Caio', '11111111111');
-echo $objEstudante->imprimeCPF();
-
+// $objEstudante = new Estudante('Caio', '11111111111');
+// echo $objEstudante->imprimeCPF();
+$obj = 10;
 // var_dump($objEstudante);
+echo "Fim do Script." . PHP_EOL;
